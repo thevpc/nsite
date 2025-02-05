6281b3379dea1ce2ee3ca57dfa194ec6810235af
thevpc <taha.bensalah@gmail.com>
2025-02-05 22:33:29 +0100

    [CHANGED] refactored repository to extract nuts tools to external git repositories

---
+++ nlib-md/src/main/java/net/thevpc/nuts/lib/md/docusaurus/DocusaurusMdParser.java
@@ -0,0 +1,15 @@
+package net.thevpc.nuts.lib.md.docusaurus;
+
+import net.thevpc.nuts.lib.md.base.BaseMdParser;
+
+import java.io.Reader;
+
+public class DocusaurusMdParser extends BaseMdParser {
+
+    public DocusaurusMdParser(Reader r) {
+        super(r);
+    }
+
+
+
+}
