bfd80aab409e45a74f9ec5f485e60a9b2567eab0
thevpc <taha.bensalah@gmail.com>
2025-08-29 04:27:14 +0100

    Update ExprNodeParserTest.java : [CHANGED] removed NLogOp and made NLog extensible

--- nsite-lib/src/test/java/test/ExprNodeParserTest.java
+++ nsite-lib/src/test/java/test/ExprNodeParserTest.java
@@ -1,15 +1,15 @@
 package test;
 
 import net.thevpc.nuts.Nuts;
-import net.thevpc.nsite.context.NDocContext;
-import net.thevpc.nsite.executor.expr.DefaultNDocExprEvaluator;
+import net.thevpc.nsite.context.NSiteContext;
+import net.thevpc.nsite.executor.expr.DefaultNSiteExprEvaluator;
 
 public class ExprNodeParserTest {
     public static void main(String[] args) {
         Nuts.openWorkspace(args).share();
         String expr = "a=b*2+6*1+m(-2)+1";
-        DefaultNDocExprEvaluator e = new DefaultNDocExprEvaluator();
-        Object d = e.eval(expr,new NDocContext());
+        DefaultNSiteExprEvaluator e = new DefaultNSiteExprEvaluator();
+        Object d = e.eval(expr,new NSiteContext());
         System.out.println(d);
 
     }
