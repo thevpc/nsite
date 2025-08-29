f7edf987a9f26c652b12107d2f26f08ffd11f48f
thevpc <taha.bensalah@gmail.com>
2025-08-29 04:27:14 +0100

    Update IncludeFct.java : [CHANGED] removed NLogOp and made NLog extensible

--- nsite-lib/src/main/java/net/thevpc/nsite/executor/expr/fct/IncludeFct.java
+++ nsite-lib/src/main/java/net/thevpc/nsite/executor/expr/fct/IncludeFct.java
@@ -3,7 +3,7 @@ package net.thevpc.nsite.executor.expr.fct;
 import net.thevpc.nuts.expr.NExprDeclarations;
 import net.thevpc.nuts.expr.NExprNodeValue;
 import net.thevpc.nuts.io.NPath;
-import net.thevpc.nsite.context.NDocContext;
+import net.thevpc.nsite.context.NSiteContext;
 import net.thevpc.nsite.executor.expr.BaseNexprNExprFct;
 import net.thevpc.nsite.util.FileProcessorUtils;
 import net.thevpc.nsite.util.StringUtils;
@@ -24,7 +24,7 @@ public class IncludeFct extends BaseNexprNExprFct {
         if (args.size() != 1) {
             throw new IllegalStateException(name + " : invalid arguments count");
         }
-        NDocContext fcontext = fcontext(context);
+        NSiteContext fcontext = fcontext(context);
 
         String str = (String) args.get(0).getValue().orNull();
         String path = FileProcessorUtils.toAbsolute(str, fcontext.getWorkingDirRequired());
