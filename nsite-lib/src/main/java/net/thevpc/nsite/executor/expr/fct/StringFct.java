963ca019ea0bd3d5664b12c9cde14512e7dc8d48
thevpc <taha.bensalah@gmail.com>
2025-08-30 20:33:18 +0100

    Update StringFct.java : [UPDATED] reused Scoped Log

--- nsite-lib/src/main/java/net/thevpc/nsite/executor/expr/fct/StringFct.java
+++ nsite-lib/src/main/java/net/thevpc/nsite/executor/expr/fct/StringFct.java
@@ -5,7 +5,9 @@ import net.thevpc.nuts.expr.NExprNodeValue;
 import net.thevpc.nsite.context.NSiteContext;
 import net.thevpc.nsite.executor.expr.BaseNexprNExprFct;
 import net.thevpc.nsite.util.StringUtils;
+import net.thevpc.nuts.log.NLog;
 import net.thevpc.nuts.util.NLiteral;
+import net.thevpc.nuts.util.NMsg;
 
 import java.util.List;
 
@@ -19,10 +21,8 @@ public class StringFct extends BaseNexprNExprFct {
         if (args.size() != 1) {
             throw new IllegalStateException(name + " : invalid arguments count");
         }
-        NSiteContext fcontext = fcontext(context);
-
         String str = (String) args.get(0).getValue().orNull();
-        fcontext.getLog().debug("eval", name + "(" + StringUtils.toLiteralString(str) + ")");
+        NLog.ofScoped(getClass()).debug(NMsg.ofC("[%] %s(%s)","eval",name,StringUtils.toLiteralString(str)));
         return NLiteral.of(str).toStringLiteral();
     }
 }
