8d3d2e3ed5bde3cd99192a4c961925f373127395
thevpc <taha.bensalah@gmail.com>
2025-09-22 12:26:17 +0100

    Update NHtmlAttr.java : fixed code snippets generation

---
+++ nsite-lib/src/main/java/net/thevpc/nsite/html/NHtmlAttr.java
@@ -0,0 +1,11 @@
+package net.thevpc.nsite.html;
+
+public class NHtmlAttr {
+    String name;
+    String value;
+
+    public NHtmlAttr(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+}
