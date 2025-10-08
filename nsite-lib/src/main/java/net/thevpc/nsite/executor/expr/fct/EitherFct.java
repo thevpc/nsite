897155f253c22a19dfadd5099a83f61a877038a2
thevpc <taha.bensalah@gmail.com>
2025-10-08 23:49:46 +0100

    Update EitherFct.java : refactor NMsg

--- nsite-lib/src/main/java/net/thevpc/nsite/executor/expr/fct/EitherFct.java
+++ nsite-lib/src/main/java/net/thevpc/nsite/executor/expr/fct/EitherFct.java
@@ -1,13 +1,9 @@
 package net.thevpc.nsite.executor.expr.fct;
 
 import net.thevpc.nsite.executor.expr.BaseNexprNExprFct;
-import net.thevpc.nsite.util.StringUtils;
 import net.thevpc.nuts.expr.NExprDeclarations;
 import net.thevpc.nuts.expr.NExprNodeValue;
-import net.thevpc.nuts.log.NLog;
 import net.thevpc.nuts.util.NBlankable;
-import net.thevpc.nuts.util.NLiteral;
-import net.thevpc.nuts.util.NMsg;
 
 import java.util.List;
 
